8bd722edbc954f387593f8e24c225453255b99f9
chris-huynh <41095524+chris-huynh@users.noreply.github.com>
2019-10-02 22:45:30 -0400

    Create POST and GET routes for workshops

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,49 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+
+admin.initializeApp();
 
 // Create and Deploy Your First Cloud Functions
 // https://firebase.google.com/docs/functions/write-firebase-functions
 
 exports.helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
+    response.send("Hello from Firebase!");
+});
+
+exports.getWorkshops = functions.https.onRequest((request, response) => {
+    admin.firestore().collection('workshops').get()
+        .then(data => {
+            let workshops = [];
+            data.forEach(doc => {
+                workshops.push(doc.data());
+            });
+            return response.json(workshops)
+        })
+        .catch(error => console.error(error))
+});
+
+exports.createWorkshop = functions.https.onRequest((request, response) => {
+    if (request.method !== 'POST') {
+        return response.status(400).json({error: 'Method not allowed'})
+    }
+    const newWorkshop = {
+        session: request.body.session,
+        workshopTitle: request.body.workshopTitle,
+        maxCapacity: request.body.maxCapacity,
+        students: request.body.students,
+        currentCapacity: request.body.currentCapacity,
+        workshopDescription: request.body.workshopDescription,
+        presenterName: request.body.presenterName
+    };
+
+    admin.firestore()
+        .collection('workshops')
+        .add(newWorkshop)
+        .then(doc => {
+            response.json({message: 'document ${doc.id} successfully created'});
+        })
+        .catch((error) => {
+            response.status(500).json({error: 'Something went wrong.'});
+            console.error(error)
+        });
 });
