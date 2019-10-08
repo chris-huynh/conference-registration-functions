7aac8abb5eb2eab6c7ed252cd622759ca4ba9cb3
chris-huynh <41095524+chris-huynh@users.noreply.github.com>
2019-10-08 17:08:45 -0400

    Add schema for users and workshops collection

---
+++ dbschema.js
@@ -0,0 +1,38 @@
+let db = {
+    users: [
+        {
+            userId: "string",
+            email: "name@email.com",
+            firstName: "string",
+            lastName: "string",
+            createdAt: "ISO String",
+            imageUrl: "URL string",
+            university: "string",
+            major: "string",
+            graduationYear: "string",
+            genderPronouns: "string",
+            family: "string",
+            twitter: "string",
+            instagram: "string",
+            facebook: "string",
+            snapchat: "string",
+            phoneNumber: "string",
+            banquetEntree: "string",
+            foodAllergies: "string",
+            shirtSize: "string",
+            emergencyContactName: "string",
+            emergencyContactPhone: "string",
+            emergencyContactRelationship: "string"
+        }
+    ],
+    workshops: [
+        {
+            workshopTitle: "string",
+            workshopDescription: "string",
+            presenterName: "string",
+            students: [ "string", "string" ],
+            session: 1,
+            maxCapacity: 10
+        }
+    ]
+};
